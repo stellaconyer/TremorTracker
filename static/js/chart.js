2cb688a191f1d81168860a1ed9218b89dc39016a
Stella Cotton <stella@stellacotton.com>
2013-11-28 11:28:43 -0800

    Add x-axis to heatmap, need to move and add labels

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,13 +1,15 @@
-// SVG width and height
-// var dataset = [{'timestamp': '1385407616347', 'data': [2.4728413708669374, 1.0213275879824053, 0.6079034848067788, 0.77700792781258421]}, {'timestamp': '1385407617347', 'data': [0.5159543321896326, 0.30089637877501274, 0.29576087039073845, 0.17047443811929824]}, {'timestamp': '1385407615180', 'data': [0.22355068543427009, 0.1828661683728921, 0.27317129983999899, 0.25205675288116147]}, {'timestamp': '1385407613180', 'data': [0.27430178945792527, 0.17270123904337203, 0.16406304991091852, 0.18666311418691917]}, {'timestamp': '1385407614180', 'data': [0.24340115857097588, 0.13899838286602154, 0.17379015269958525, 0.16380843391672484]}];
 
+var dataset = [{'timestamp': '1385407616347', 'data': [2.4728413708669374, 1.0213275879824053, 0.6079034848067788, 0.77700792781258421]}, {'timestamp': '1385407617347', 'data': [0.5159543321896326, 0.30089637877501274, 0.29576087039073845, 0.17047443811929824]}, {'timestamp': '1385407615180', 'data': [0.22355068543427009, 0.1828661683728921, 0.27317129983999899, 0.25205675288116147]}, {'timestamp': '1385407613180', 'data': [0.27430178945792527, 0.17270123904337203, 0.16406304991091852, 0.18666311418691917]}, {'timestamp': '1385407614180', 'data': [0.24340115857097588, 0.13899838286602154, 0.17379015269958525, 0.16380843391672484]}];
+
+
+// SVG width and height
 var margin = {top: 30, right: 20, bottom: 30, left: 50},
     w = 1000 - margin.left - margin.right,
     h = 500 - margin.top - margin.bottom;
 
 
-    var w = 1000;
-    var h = 500;
+    var w = 500;
+    var h = 300;
 
 //Create array of data to determine max and min values
     function createArrayFromJSON (dataset) {
@@ -38,6 +40,14 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
       .attr("width", w)
       .attr("height", h);
 
+// function(d) { return d.date; } returns all the ‘date’ values in ‘data’
+// The .extent function that finds the maximum and minimum values in the array
+// The .domain function which returns those maximum and minimum values to D3 as the range for the x axis.
+
+
+
+
+
 //Create a column for each entry
     var columns = chart.selectAll("g")
       .data(dataset)
@@ -64,7 +74,7 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
     // Overlay "data" value
     rows
       .append("text")
-      .text(function (d, i) { return d.toPrecision(5); })
+      .text(function (d, i) { return d.toPrecision(3); })
       .attr("dx", "20px")
       .attr("dy", "14px")
       .attr("font-family", "sans-serif")
@@ -77,6 +87,21 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
       .on('mouseout', function(d){
           var nodeSelection = d3.select(this).style({opacity:'0'});
       });
+
+      var x = d3.time.scale().range([0, w]);
+      x.domain(d3.extent(dataset, function(d) {
+        console.log(Date(d.timestamp));
+        return Date(d.timestamp);
+      }));
+
+      var xAxis = d3.svg.axis().scale(x)
+      .orient("bottom").ticks(5);
+
+      chart.selectAll("g")
+      .append("g")                // Add the X Axis
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + h + ")")
+      .call(xAxis);
 
 
  // helper functions are super useful with d3
