3bad40d560cf8ec1f64c04768855ea687501b670
Stella Cotton <stella@stellacotton.com>
2013-11-30 12:18:32 -0800

    Add unformatted chart label and y-axis code

--- static/js/chart.js
+++ static/js/chart.js
@@ -2,14 +2,11 @@
 var dataset = [{'timestamp': '1385407616347', 'data': [2.4728413708669374, 1.0213275879824053, 0.6079034848067788, 0.77700792781258421]}, {'timestamp': '1385407617347', 'data': [0.5159543321896326, 0.30089637877501274, 0.29576087039073845, 0.17047443811929824]}, {'timestamp': '1385407615180', 'data': [0.22355068543427009, 0.1828661683728921, 0.27317129983999899, 0.25205675288116147]}, {'timestamp': '1385407613180', 'data': [0.27430178945792527, 0.17270123904337203, 0.16406304991091852, 0.18666311418691917]}, {'timestamp': '1385407614180', 'data': [0.24340115857097588, 0.13899838286602154, 0.17379015269958525, 0.16380843391672484]}];
 
 
+
 // SVG width and height
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    w = 1000 - margin.left - margin.right,
+var margin = {top: 30, right: 20, bottom: 30, left: 20},
+    w = 500 - margin.left - margin.right,
     h = 500 - margin.top - margin.bottom;
-
-
-    var w = 500;
-    var h = 300;
 
 //Create array of data to determine max and min values
     function createArrayFromJSON (dataset) {
@@ -34,17 +31,12 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
       .domain([max, min])
       .range(["#fdd", "#f00"]);
 
+
 //Create an SVG element
     var chart = d3.select("#chart")
       .append("svg")
-      .attr("width", w)
-      .attr("height", h);
-
-// function(d) { return d.date; } returns all the ‘date’ values in ‘data’
-// The .extent function that finds the maximum and minimum values in the array
-// The .domain function which returns those maximum and minimum values to D3 as the range for the x axis.
-
-
+      .attr("width", w + margin.left + margin.right)
+      .attr("height", h + margin.top + margin.bottom);
 
 
 
@@ -53,7 +45,7 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
       .data(dataset)
       .enter()
       .append("g")
-      .attr("transform", function (d, i) { return translate(i * 60, 0); });
+      .attr("transform", function (d, i) { return translate((i * 60)+60, 0); });
 
 
 //Create rows for each value in "data"
@@ -61,7 +53,7 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
       .data(function (d, i) { return d.data; })
       .enter()
       .append("g")
-      .attr("transform", function (d, i) { return translate(0, i * 20); });
+      .attr("transform", function (d, i) { return translate(0, i * 20+20); });
 
     //Create a rectangle each of the four PSD readings 
     rows
@@ -69,7 +61,6 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
       .attr("width", 58)
       .attr("height", 18)
       .attr("fill", colorScale);
-
 
     // Overlay "data" value
     rows
@@ -88,23 +79,60 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
           var nodeSelection = d3.select(this).style({opacity:'0'});
       });
 
-      var x = d3.time.scale().range([0, w]);
-      x.domain(d3.extent(dataset, function(d) {
-        console.log(Date(d.timestamp));
-        return Date(d.timestamp);
-      }));
 
-      var xAxis = d3.svg.axis().scale(x)
-      .orient("bottom").ticks(5);
+// Create x and y axis labels
+  //Take in timestamp as string, convert to JS Date object
+  var convertTimestamp = function (timestamp) {
+    return new Date(parseInt(timestamp, 10));
+  };
 
-      chart.selectAll("g")
-      .append("g")                // Add the X Axis
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + h + ")")
-      .call(xAxis);
+  //Format a timestamp to H:M:S
+  var formatTime = d3.time.format("%X");
+
+  //Format a timestamp to "%a %b %e %H:%M:%S %Y"
+  var formatDate = d3.time.format("%c");
 
 
- // helper functions are super useful with d3
+  //x labels as H:M:S
+  var xLabels = columns.append("g")
+    .append("text")
+    .text(function(d){return formatTime(convertTimestamp(d.timestamp));})
+    .attr("width", 58)
+    .attr("height", 18)
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "12px")
+    .attr("transform", function (d, i) {
+      return translate(0, d.data.length*20 + 12+20); });
+
+
+  var chartTitle = ['1385407614180'];
+
+  var chartLabel = chart.selectAll("g")
+    .append("g")
+    .data(chartTitle, function(d) {return d; })
+    .enter()
+    .append("text")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "12px")
+    .text(function (d,i) {return d; })
+    .attr("transform", function (d, i) {return translate(0, i+15); });
+
+  //y labels
+
+  var yValues = ["1hz", "3hz", "6hz", "10hz"];
+
+  var yLabels = chart.selectAll("g")
+    .append("g")
+    .data(yValues, function(d) {return d; })
+    .enter()
+    .append("text")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "12px")
+    .text(function (d) {console.log(d); return d;});
+
+
+
+ // helper function for translating
     function translate (x, y) {
       return ['translate(', x, ',', y, ')'].join('');
     }
--- static/js/chart2.js
+++ static/js/chart2.js
@@ -1,27 +1,32 @@
 // SVG width and height
 
 var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    w = 1000 - margin.left - margin.right,
+    w = 400 - margin.left - margin.right,
     h = 500 - margin.top - margin.bottom;
 
 
-    var w = 1000;
-    var h = 500;
+    // var w = 400;
+    // var h = 78;
 
-var x = d3.time.scale().range([0, w]);
-var y = d3.scale.linear().range([h, 0]);
 
-var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+// var valueline = d3.svg.line()
+//     .x(function(d) { return x(d.timestamp); });
 
-var yAxis = d3.svg.axis().scale(y)
-    .orient("left").ticks(5);
+    var dataset = [{'timestamp': '1385407616347', 'data': [2.4728413708669374, 1.0213275879824053, 0.6079034848067788, 0.77700792781258421]}, {'timestamp': '1385407617347', 'data': [0.5159543321896326, 0.30089637877501274, 0.29576087039073845, 0.17047443811929824]}, {'timestamp': '1385407615180', 'data': [0.22355068543427009, 0.1828661683728921, 0.27317129983999899, 0.25205675288116147]}, {'timestamp': '1385407613180', 'data': [0.27430178945792527, 0.17270123904337203, 0.16406304991091852, 0.18666311418691917]}, {'timestamp': '1385407614180', 'data': [0.24340115857097588, 0.13899838286602154, 0.17379015269958525, 0.16380843391672484]}];
 
-var valueline = d3.svg.line()
-    .x(function(d) { return x(d.timestamp); });
 
-    var dataset = [{'timestamp': '1385407616347', 'data': [2.4728413708669374, 1.0213275879824053, 0.6079034848067788, 0.77700792781258421]}, {'timestamp': '1385407617347', 'data': [0.5159543321896326, 0.30089637877501274, 0.29576087039073845, 0.17047443811929824]}, {'timestamp': '1385407615180', 'data': [0.22355068543427009, 0.1828661683728921, 0.27317129983999899, 0.25205675288116147]}, {'timestamp': '1385407613180', 'data': [0.27430178945792527, 0.17270123904337203, 0.16406304991091852, 0.18666311418691917]}, {'timestamp': '1385407614180', 'data': [0.24340115857097588, 0.13899838286602154, 0.17379015269958525, 0.16380843391672484]}]
+// function(d) { return d.date; } returns all the ‘date’ values in ‘data’
+// The .extent function that finds the maximum and minimum values in the array
+// The .domain function which returns those maximum and minimum values to D3 as the range for the x axis.
 
+      // var x = d3.time.scale().range([0, w]);
+      // x.domain(d3.extent(dataset, function(d) {
+      //   console.log(Date(d.timestamp));
+      //   return Date(d.timestamp);
+      // }));
+
+      // var xAxis = d3.svg.axis().scale(x)
+      // .orient("bottom").ticks(5);
 
 
 
@@ -48,11 +53,25 @@ var valueline = d3.svg.line()
       .domain([max, min])
       .range(["#fdd", "#f00"]);
 
+
+      var axisScale = d3.scale.linear()
+        .domain([0,400])
+        .range([0,400]);
+
+      var xAxis = d3.svg.axis()
+        .scale(axisScale);
+
+
+
+
 //Create an SVG element
     var chart = d3.select("#chart")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
+
+
+
 
 //Create a column for each entry
     var columns = chart.selectAll("g")
@@ -94,6 +113,8 @@ var valueline = d3.svg.line()
           var nodeSelection = d3.select(this).style({opacity:'0'});
       });
 
+          var xAxisGroup = chart.append("g")
+        .call(xAxis);
 
  // helper functions are super useful with d3
     function translate (x, y) {
